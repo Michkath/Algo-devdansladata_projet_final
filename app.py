0cfd2aac2c1a4893bf1f9c1b1361a3c87e82be37
gassa <goundo.diallo@universite-paris-saclay.fr>
2026-04-16 22:04:45 +0200

    Dernière mise à jour

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
-from routes import api  
+from routes import api
+from flask_cors import CORS  
 
 app = Flask(__name__)
-app.json.ensure_ascii = False 
+app.json.ensure_ascii = False
+
+CORS(app) 
 
 app.register_blueprint(api)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
